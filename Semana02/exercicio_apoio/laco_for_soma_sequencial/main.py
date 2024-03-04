50e169bdf2fa801d65a258f011a75fe1cd262cd3
RosaMaster <edu.wd2014@gmail.com>
2024-03-04 00:31:12 -0300

    feat: include slides e estruturada diretorios

---
+++ Semana02/exercicio_apoio/laco_for_soma_sequencial/main.py
@@ -0,0 +1,5 @@
+'''
+Desenvolver os diagramas de blocos e as codificações em português estruturado usando
+laço incondicional (para) do seguinte problema: Construir um programa que apresente a
+soma dos cem primeiros números naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
+'''
